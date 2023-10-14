07f8e481078c9e3c36b0142dd0a72b030ce43ddc
IVANCCAMA <ccamaivan@gmail.com>
2023-10-13 22:49:40 -0400

    proyecto backend iniciado

---
+++ app/config/db.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    HOST: "containers-us-west-109.railway.app",
+    USER: "postgres",
+    PASSWORD: "3NrdUh8AYorUGcWEZRea",
+    DB: "railway",
+    dialect: "postgres",
+    port: "6479", 
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  };
+  
+  
---
+++ app/models/tutorial.models.js
@@ -0,0 +1,15 @@
+module.exports = (sequelize, Sequelize) => {
+    const Tutorial = sequelize.define("tutorial", {
+      user: {
+        type: Sequelize.STRING
+      },
+      tipo_user: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      }
+    });
+  
+    return Tutorial;
+  };
