09c4e16bfb5e3d096bfa459b5daccaf6e0a74412
Jhoselyn Ossio Mamani <ossiomjhossy@gmail.com>
2023-10-13 23:29:42 -0400

    Commit de Jhossy

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.send('Hola mundo de apis xxd, holis awis');
+  res.send('Hola mundo de apis xxd, holis Ivimon');
 });
 
 app.listen(PORT, () => {
